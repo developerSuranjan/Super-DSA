da78daa11bdbd7bc693218c00889a8a8c74cbc08
SURANJAN <sdclouddevelopment@gmail.com>
2023-08-25 21:27:23 +0530

    new step

--- method_1.cpp
+++ method_1.cpp
@@ -7,8 +7,8 @@ public:
         prefix[0]=1;
         suffix[n-1]=1;
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*nums[i-1];
-            suffix[n-i-1]=suffix[n-i]*nums[n-i];
+            prefix[i]=prefix[i-1]*nums[i-1]; //prefix product
+            suffix[n-i-1]=suffix[n-i]*nums[n-i]; //suffix product
         }
         vector<int> ans;
         for(int i=0;i<n;i++){
